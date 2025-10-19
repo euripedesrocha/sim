f47a1c1ed451af506e54850ed5dc5b9daa0fdfc5
Template Janitor <template.janitor@example.com>
2025-10-19 07:52:17 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -7,7 +7,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(sim_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND NOT WIN32)
 
     message(STATUS "Sanity checking UndefinedBehaviorSanitizer, it should be supported on this platform")
@@ -54,183 +54,183 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(sim_setup_options)
+  option(sim_ENABLE_HARDENING "Enable hardening" ON)
+  option(sim_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    sim_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    sim_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  sim_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR sim_PACKAGING_MAINTAINER_MODE)
+    option(sim_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(sim_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(sim_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(sim_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(sim_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(sim_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(sim_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(sim_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(sim_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(sim_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(sim_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(sim_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(sim_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(sim_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(sim_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(sim_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(sim_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(sim_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(sim_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(sim_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(sim_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(sim_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(sim_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(sim_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(sim_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(sim_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_USER_LINKER
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      sim_ENABLE_IPO
+      sim_WARNINGS_AS_ERRORS
+      sim_ENABLE_USER_LINKER
+      sim_ENABLE_SANITIZER_ADDRESS
+      sim_ENABLE_SANITIZER_LEAK
+      sim_ENABLE_SANITIZER_UNDEFINED
+      sim_ENABLE_SANITIZER_THREAD
+      sim_ENABLE_SANITIZER_MEMORY
+      sim_ENABLE_UNITY_BUILD
+      sim_ENABLE_CLANG_TIDY
+      sim_ENABLE_CPPCHECK
+      sim_ENABLE_COVERAGE
+      sim_ENABLE_PCH
+      sim_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  sim_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (sim_ENABLE_SANITIZER_ADDRESS OR sim_ENABLE_SANITIZER_THREAD OR sim_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(sim_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(sim_global_options)
+  if(sim_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    sim_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  sim_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(sim_ENABLE_HARDENING AND sim_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR sim_ENABLE_SANITIZER_UNDEFINED
+       OR sim_ENABLE_SANITIZER_ADDRESS
+       OR sim_ENABLE_SANITIZER_THREAD
+       OR sim_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${sim_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${sim_ENABLE_SANITIZER_UNDEFINED}")
+    sim_enable_hardening(sim_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(sim_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(sim_warnings INTERFACE)
+  add_library(sim_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  sim_set_project_warnings(
+    sim_warnings
+    ${sim_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
     "")
 
-  if(myproject_ENABLE_USER_LINKER)
+  if(sim_ENABLE_USER_LINKER)
     include(cmake/Linker.cmake)
-    myproject_configure_linker(myproject_options)
+    sim_configure_linker(sim_options)
   endif()
 
   include(cmake/Sanitizers.cmake)
-  myproject_enable_sanitizers(
-    myproject_options
-    ${myproject_ENABLE_SANITIZER_ADDRESS}
-    ${myproject_ENABLE_SANITIZER_LEAK}
-    ${myproject_ENABLE_SANITIZER_UNDEFINED}
-    ${myproject_ENABLE_SANITIZER_THREAD}
-    ${myproject_ENABLE_SANITIZER_MEMORY})
+  sim_enable_sanitizers(
+    sim_options
+    ${sim_ENABLE_SANITIZER_ADDRESS}
+    ${sim_ENABLE_SANITIZER_LEAK}
+    ${sim_ENABLE_SANITIZER_UNDEFINED}
+    ${sim_ENABLE_SANITIZER_THREAD}
+    ${sim_ENABLE_SANITIZER_MEMORY})
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(sim_options PROPERTIES UNITY_BUILD ${sim_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(sim_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      sim_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(sim_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    sim_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(sim_ENABLE_CLANG_TIDY)
+    sim_enable_clang_tidy(sim_options ${sim_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(sim_ENABLE_CPPCHECK)
+    sim_enable_cppcheck(${sim_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(sim_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    sim_enable_coverage(sim_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(sim_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(sim_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(sim_ENABLE_HARDENING AND NOT sim_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR sim_ENABLE_SANITIZER_UNDEFINED
+       OR sim_ENABLE_SANITIZER_ADDRESS
+       OR sim_ENABLE_SANITIZER_THREAD
+       OR sim_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    sim_enable_hardening(sim_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
